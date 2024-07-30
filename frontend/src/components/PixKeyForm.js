dbb9a2a42995372316c2ae64496717ab4d438341
Luiz Felipe de Castro Vilas Boas <luizfelipecastrovb@gmail.com>
2024-07-30 11:29:30 -0300

    Add PIX key form to the dashboard

---
+++ frontend/src/components/PixKeyForm.js
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+import axios from "axios";
+import { useMessage } from "../context/MessageContext";
+import { FaPix } from "react-icons/fa6";
+
+const PixKeyForm = () => {
+  const [pixKey, setPixKey] = useState("");
+  const { setMessage } = useMessage();
+
+  const handlePixKeyCreation = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post(
+        "http://localhost:8000/pix_keys",
+        { key: pixKey },
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        }
+      );
+
+      if (response.status === 201) {
+        setMessage("success", "Chave PIX criada com sucesso.");
+      } else {
+        setMessage("error", response.data.message);
+      }
+    } catch (error) {
+      setMessage("error", "Erro ao criar a chave PIX.");
+    }
+  };
+
+  return (
+    <div className="bg-white p-6 rounded-lg shadow-lg mt-6">
+      <h3 className="text-2xl font-extrabold mb-6 text-gray-900">
+        Criar Chave PIX
+      </h3>
+      <form onSubmit={handlePixKeyCreation}>
+        <div className="mb-4">
+          <label className="block text-gray-700 font-semibold">
+            <FaPix className="inline-block text-green-600 mr-2" />
+            Chave PIX
+          </label>
+          <input
+            type="text"
+            className="w-full p-2 border border-gray-300 rounded mt-2"
+            placeholder="Sua nova chave PIX"
+            value={pixKey}
+            onChange={(e) => setPixKey(e.target.value)}
+            required
+          />
+        </div>
+        <button
+          type="submit"
+          className="w-full bg-blue-600 text-white p-3 rounded-lg hover:bg-blue-700 transition duration-300"
+        >
+          Criar Chave
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default PixKeyForm;
--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -1,47 +1,32 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Header from "../components/Header";
 import UserInfo from "../components/UserInfo";
 import TransferForm from "../components/TransferForm";
 import { useMessage } from "../context/MessageContext";
 import Alert from "../components/Alert";
-import axios from "axios";
+import PixKeyForm from "../components/PixKeyForm";
 
 const Dashboard = () => {
-  const { message, setMessage } = useMessage();
-  const [userInfo, setUserInfo] = useState({
-    username: "",
-    email: "",
-    balance: 0,
-  });
-
-  useEffect(() => {
-    const fetchUserInfo = async () => {
-      try {
-        const response = await axios.get("http://localhost:8000/me", {
-          headers: {
-            Authorization: `Bearer ${localStorage.getItem("token")}`,
-          },
-        });
-        setUserInfo(response.data);
-      } catch (error) {
-        setMessage("error", "Erro ao buscar informações do usuário.");
-      }
-    };
-
-    fetchUserInfo();
-  }, [setMessage]);
+  const message = useMessage();
 
   return (
-    <div className="min-h-screen bg-gray-100">
+    <div className="min-h-screen bg-gradient-to-r from-green-400 via-blue-500 to-purple-500">
       <Header />
       <div className="container mx-auto p-4">
         {message.text && <Alert type={message.type} text={message.text} />}
         <div className="flex flex-col md:flex-row md:space-x-4">
-          <div className="md:w-1/3">
-            <UserInfo user={userInfo} />
+          <div className="md:w-1/2 gap-5">
+            <div className="md:h-fit bg-white px-6 rounded-lg shadow-lg">
+              <UserInfo />
+            </div>
+            <div className="md:h-fit bg-white px-6 rounded-lg shadow-lg">
+              <PixKeyForm />
+            </div>
           </div>
-          <div className="md:w-2/3">
-            <TransferForm />
+          <div className="md:w-1/2">
+            <div className="md:h-fit bg-white px-6 rounded-lg shadow-lg">
+              <TransferForm />
+            </div>
           </div>
         </div>
       </div>
