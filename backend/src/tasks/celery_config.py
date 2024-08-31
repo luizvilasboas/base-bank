b5b2045079a56b74554fedf00a19ac48076888d9
Luiz Felipe de Castro Vilas Boas <luizfelipecastrovb@gmail.com>
2024-08-31 18:47:07 -0300

    Update celery config to use env vars

--- backend/src/tasks/celery_config.py
+++ backend/src/tasks/celery_config.py
@@ -4,5 +4,5 @@ import os
 import tasks.transaction_tasks
 
 celery_app = Celery(__name__)
-celery_app.conf.broker_url = "redis://redis:6379/0"
-celery_app.conf.result_backend = "redis://redis:6379/0"
+celery_app.conf.broker_url = os.environ.get("CELERY_BROKER_URL", "redis://redis:6379/0")
+celery_app.conf.result_backend = os.environ.get("CELERY_RESULT_BACKEND", "redis://localhost:6379")
